678e1f6f288c80b80fa88662aabc83b634c733a4
unkn76 <97539079+unkn76@users.noreply.github.com>
2025-04-08 09:50:26 +0000

    React

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,17 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const vtrident = {
+  name: "Voron Trident",
+  mainColor: "Green",
+  secondColor: "Natural",
+  materialType: "FDM",
+  dimSize: {
+    xaxis: 250,
+    yaxis: 250,
+    zaxis: 250,
+  },
+};
+
+console.log(vtrident.dimSize.zaxis);
